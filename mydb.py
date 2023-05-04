d0d1d4da54900fb80563d132dfe0675fa4a6735d
DevIvo81 <izpy81@gmail.com>
2023-05-04 10:31:07 +0200

    second

---
+++ mydb.py
@@ -0,0 +1,22 @@
+# Install MySQL on your computer
+# https://dev.mysql.com/downloads/installer
+# pip install mysql
+# pip install mysql-connector
+# pip install mysql-connector-python
+
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='630350'
+)
+
+# prepare a cursor object
+
+cursorObject = dataBase.cursor()
+
+# Create a database
+cursorObject.execute("CREATE DATABASE IF NOT EXISTS izpyco")
+
+print("\nAll Done!\n")
